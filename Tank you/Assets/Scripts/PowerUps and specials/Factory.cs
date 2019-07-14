16100db13655bfb7ace859542756645c8b021234
Jasper <j.bonefaas@student.avans.nl>
2019-07-14 15:46:31 +0100

    Factory prefab
    Misschien nog laten stoppen als alle tanks dood zijn

---
+++ Tank you/Assets/Scripts/PowerUps and specials/Factory.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Factory : MonoBehaviour
+{
+    private float spawnTime;
+
+    [SerializeField]
+    private Transform spawnPos;
+
+    [SerializeField]
+    private GameObject tank;
+
+    [SerializeField]
+    private float spawnDelay;
+
+    [SerializeField]
+    private AudioSource sound;
+
+    void Start()
+    {
+        spawnTime = Time.time;
+    }
+
+    void Update()
+    {
+        if(Time.time >= spawnTime)
+        {
+            Instantiate(tank, spawnPos.position, spawnPos.rotation);
+            sound.Play();
+            spawnTime = Time.time + spawnDelay;
+        }
+    }
+}
